7628a34fa982ad26155e26f7f6f77c193af6c61d
Andrew Cauchi <drewcauchi@gmail.com>
2024-05-26 14:39:38 +0200

    706-26052024

---
+++ design_hashmap.js
@@ -0,0 +1,35 @@
+
+class MyHashMap {
+    constructor() {
+        this.map = [];
+    }
+
+    put(key, value) {
+        this.map[key] = value;
+    }
+
+    get(key) {
+        return this.map[key] !== undefined ? this.map[key] : -1;
+    }
+
+    remove(key) {
+        delete this.map[key];
+    }
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
+
+// Test cases
+var obj = new MyHashMap();
+obj.put(1, 1);
+console.log(obj);
+var param_2 = obj.get(2)
+console.log(param_2);
+obj.remove(1);
+console.log(obj);
